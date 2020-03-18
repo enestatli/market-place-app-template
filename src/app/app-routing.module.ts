79988d6956b5fbf295486e5bc1f26749c980f1b0
enestatli <43743872+enestatli@users.noreply.github.com>
2020-03-18 23:37:34 +0300

    added guide page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: "",
+    loadChildren: () => import("./tabs/tabs.module").then(m => m.TabsPageModule)
+  },
+  {
+    path: "guide",
+    loadChildren: () =>
+      import("./pages/guide/guide.module").then(m => m.GuidePageModule)
   }
 ];
 @NgModule({
