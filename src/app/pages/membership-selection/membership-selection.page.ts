cbd62e89b3bec2c868292e64193c8f1e4ffbd389
enestatli <43743872+enestatli@users.noreply.github.com>
2020-03-19 21:01:24 +0300

    added membership-selection page

--- src/app/pages/membership-selection/membership-selection.page.ts
+++ src/app/pages/membership-selection/membership-selection.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NavController } from "@ionic/angular";
+import { allRoutes } from "src/app/models/common-models";
 
 @Component({
-  selector: 'app-membership-selection',
-  templateUrl: './membership-selection.page.html',
-  styleUrls: ['./membership-selection.page.scss'],
+  selector: "app-membership-selection",
+  templateUrl: "./membership-selection.page.html",
+  styleUrls: ["./membership-selection.page.scss"]
 })
 export class MembershipSelectionPage implements OnInit {
+  constructor(public navCtrl: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  goClientRegister() {
+    this.navCtrl.navigateRoot(allRoutes.settings); //clientRegister
   }
 
+  goMemberRegister() {
+    this.navCtrl.navigateRoot(allRoutes.settings); //memberRegister
+  }
 }
